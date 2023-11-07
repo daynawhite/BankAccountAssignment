11b18469553697f82c9200ab3d4f193890f4e27e
daynawhite <dayna26.2@gmail.com>
2023-11-07 14:44:16 -0600

    finished code; need to test)

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 
 let currentBalance = 0
+let transactDate
+let newCharge
+let newDeposit
 
 class BankAccount {
     constructor (accountNumber, owner) {
@@ -10,27 +13,32 @@ class BankAccount {
     
     balance() {
         // compute balance by adding up amounts in transaction array
-        // currentBalance = 
+        currentBalance = this.transactions.reduce(getSum)
+        function getSum(total, value) {
+            return total + value;
+        }
+        console.log(currentBalance)
         return currentBalance
     }
     deposit(depAmt) {
         // creates a new transaction representing the deposit, and adds it to the transactions array
-        
+        newDeposit = new Transaction(depAmt)
+        this.transactions.push(depAmt)
     }
     charge(payee, amt) {
         // creates a new transaction with payee and amt;
-        
+        newCharge = new Transaction(amt, payee)
         // adds the new transaction to the transaction array
+        this.transactions.push(amt)
     }
 }
-
 class Transaction {
     constructor (amount, payee) {
         this.amount = amount
         this.payee = payee
-        let transactDate = new Date()
+        this.transactDate = new Date()
     }
 }
-const trans1 = new Transaction(1000, 'paycheck')
-const trans2 = new Transaction(-200, 'HEB')
-const trans3 = new Transaction(-50, 'Amazon')
+// const trans1 = new Transaction(1000, 'paycheck')
+// const trans2 = new Transaction(-200, 'HEB')
+// const trans3 = new Transaction(-50, 'Amazon')
